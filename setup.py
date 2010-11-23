d113610d739d3a84bbf8b5093538576047a3169f
Travis Swicegood <development@domain51.com>
2010-11-23 13:24:31 -0600

    add setup.py to handle base installation

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='armstrong.base',
+    version='0.1',
+    description='Base functionality that needs to be shared widely',
+    author='Texas Tribune',
+    author_email='tech@texastribune.org',
+    url='http://github.com/texastribune/armstrong.base/',
+    packages=[
+        'armstrong',
+        'armstrong.base',
+    ],
+
+    install_requires=[
+        'setuptools',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
