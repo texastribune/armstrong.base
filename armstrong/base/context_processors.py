841278b05c81115b9b265d4d7d45bf0cef2af7df
Travis Swicegood <development@domain51.com>
2010-11-29 13:24:39 -0600

    Add ability to retrieve UPLOADED_MEDIA_URL as well
    This adds the UPLOADED_MEDIA_URL and the SECURE_UPLOADED_MEDIA_URL
    values.  This assumes that for SECURE_UPLOADED_MEDIA_URL to be present
    that UPLOADED_MEDIA_URL will also be present.  Might want to revisit
    that assumption later, but for now it's safe.

--- armstrong/base/context_processors.py
+++ armstrong/base/context_processors.py
@@ -1,7 +1,13 @@
 from django.conf import settings
 
 def media_url(request):
-    if request.is_secure:
-        return {'MEDIA_URL': settings.SECURE_MEDIA_URL}
-    return {'MEDIA_URL': settings.MEDIA_URL}
+    context = {}
+    context['MEDIA_URL'] = request.is_secure and settings.SECURE_MEDIA_URL or \
+            settings.MEDIA_URL
+    if hasattr(settings, 'UPLOADED_MEDIA_URL'):
+        context['UPLOADED_MEDIA_URL'] = request.is_secure and \
+            settings.SECURE_UPLOADED_MEDIA_URL or \
+            settings.UPLOADED_MEDIA_URL
+
+    return context
 
--- armstrong/base/tests/context_processors.py
+++ armstrong/base/tests/context_processors.py
@@ -23,6 +23,15 @@ class TestOfMediaUrlContextProcessor(TestCase):
             fake_settings.has_attr(MEDIA_URL=expected_media_url)
         return fake_settings, expected_result
 
+    def generate_fake_settings_and_result_with_uploaded(self, is_secure=False):
+        fake_settings, expected_result = self.generate_fake_settings_and_result(is_secure=is_secure)
+        expected_uploaded_media_url = expected_result['MEDIA_URL'] + "/uploads/"
+        fake_settings.has_attr(UPLOADED_MEDIA_URL=expected_uploaded_media_url)
+        if is_secure:
+            fake_settings.has_attr(SECURE_UPLOADED_MEDIA_URL=expected_uploaded_media_url)
+        expected_result['UPLOADED_MEDIA_URL'] = expected_uploaded_media_url
+        return fake_settings, expected_result
+
     def test_returns_media_url_from_settings(self):
         request = self.generate_fake_request()
         fake_settings, expected_result = self.generate_fake_settings_and_result()
@@ -32,6 +41,18 @@ class TestOfMediaUrlContextProcessor(TestCase):
     def test_returns_secure_media_url_from_settings_on_is_secure(self):
         request = self.generate_fake_request(is_secure=True)
         fake_settings, expected_result = self.generate_fake_settings_and_result(is_secure=True)
+        with fudge.patched_context(context_processors, 'settings', fake_settings):
+            self.assertEquals(media_url(request), expected_result)
+
+    def test_includes_uploaded_media_url_if_present(self):
+        request = self.generate_fake_request()
+        fake_settings, expected_result = self.generate_fake_settings_and_result_with_uploaded()
+        with fudge.patched_context(context_processors, 'settings', fake_settings):
+            self.assertEquals(media_url(request), expected_result)
+
+    def test_includes_secure_uploaded_media_url_if_present_and_is_secure(self):
+        request = self.generate_fake_request(is_secure=True)
+        fake_settings, expected_result = self.generate_fake_settings_and_result_with_uploaded(is_secure=True)
         with fudge.patched_context(context_processors, 'settings', fake_settings):
             self.assertEquals(media_url(request), expected_result)
 
